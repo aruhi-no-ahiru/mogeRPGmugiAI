3a48894aee51fb53a6052c68bd6337c3a7ef47a9
blackduckn <g07mk014@gmail.com>
2017-11-25 10:34:45 +0900

    むぎのかしこさが1あがった

--- ai_mugi.c
+++ ai_mugi.c
@@ -22,6 +22,15 @@
 #define MOVE_DOWN (1)
 #define MOVE_RIGHT (2)
 #define MOVE_LEFT (3)
+#define THRESHOLD_HAMMER (8)
+#define THRESHOLD_NEAR_ITEM (5)
+//#define THRESHOLD_FAR_ITEM (15)
+#define THRESHOLD_HEAL_HP (9)
+#define THRESHOLD_HEAL_AGI (14)
+#define THRESHOLD_HEAL_STR (10)
+
+int array_threshold_far_item[11]={999,20,15,14,13,12,11,8,7,6,6};
+
 
 char map[MAP_SIZE][MAP_SIZE];
 
@@ -30,24 +39,34 @@ typedef struct queue_element_t{
 		int x;
 		int y;
 		int dir;
+		int hammer;
+		int count;
 } queue_element;
 
 queue_element queue[QUEUE_SIZE];
 int queue_rp;
 int queue_wp;
+int current_weapon_pow=0;
+int current_map_level;
 
-
-int is_heal(json_t *player, int threshold)
+int is_heal(json_t *player, float rate)
 {
 		int result=0;
 		json_int_t hp;
 		json_int_t maxhp;
 		json_int_t heal;
+		json_int_t agi;
+		json_int_t str;
 		hp=json_integer_value(json_object_get(player,"hp"));
 		maxhp=json_integer_value(json_object_get(player,"maxhp"));
 		heal=json_integer_value(json_object_get(player,"heal"));
+		agi=json_integer_value(json_object_get(player,"agi"));
+		str=json_integer_value(json_object_get(player,"str"));
 
-		if((heal>0) && ( (hp<=10) ||(hp<(maxhp/threshold))))
+		if((heal>0) && 
+			( (hp<=(THRESHOLD_HEAL_HP+maxhp/10+current_map_level/10)*rate)
+				 || (agi<=THRESHOLD_HEAL_AGI)
+				 || (str<=THRESHOLD_HEAL_STR)))
 		{
 			result=1;
 		}
@@ -73,15 +92,17 @@ int make_map(json_t* obj, int value)
 		return array_size;
 }
 
-void push(int x, int y, int dir)
+void push(int x, int y, int dir, int hammer,int count)
 {
 		queue[queue_wp].x=x;
 		queue[queue_wp].y=y;
 		queue[queue_wp].dir=dir;
+		queue[queue_wp].hammer=hammer;
+		queue[queue_wp].count=count;
 		queue_wp=(queue_wp+1)%QUEUE_SIZE;
 }
 
-int pop(int *x, int *y, int *dir)
+int pop(int *x, int *y, int *dir, int *hammer,int *count)
 {
 		if(queue_wp==queue_rp)
 			return 0;
@@ -89,78 +110,118 @@ int pop(int *x, int *y, int *dir)
 		*x=queue[queue_rp].x;
 		*y=queue[queue_rp].y;
 		*dir=queue[queue_rp].dir;
+		*hammer=queue[queue_rp].hammer;
+		*count=queue[queue_rp].count;
 		queue_rp=(queue_rp+1)%QUEUE_SIZE;
 		return 1;
 }
 
-int search(int x, int y, int target)
+int search(int x, int y, int target,int hammer, int *count)
 {
 		int find=0;
 		int dir;
+		int used_hammer;
+		int step;
 		char tmp_map[MAP_SIZE][MAP_SIZE];
 		queue_rp=0;
 		queue_wp=0;
 		memcpy(tmp_map,map,MAP_SIZE*MAP_SIZE);
-		push(x,y,-1);
-		
-		while(pop(&x,&y,&dir))
+		push(x,y,-1,hammer,0);
+
+		while(pop(&x,&y,&dir,&hammer,&step))
 		{
 //				printf("x:%d y:%d dir:%d tmp_map:%d \n",x,y,dir,tmp_map[x][y]);
 
 				if(tmp_map[x][y]==target)
 				{
 					find=1;
+					*count=step;
 					break;
 				}
 				tmp_map[x][y]=WALL;
-				if(tmp_map[x-1][y]==FREESPACE || tmp_map[x-1][y]==target)
+				used_hammer=0;
+				if(tmp_map[x-1][y]==FREESPACE || tmp_map[x-1][y]==target || ( tmp_map[x-1][y]==BLOCK && hammer>0))
 				{
 //						printf("PUSH LEFT\n");
-						if(dir==-1)
+						if(tmp_map[x-1][y]==BLOCK)
 						{
-								push(x-1,y,MOVE_LEFT);
+								used_hammer=1;
 						}
 						else
 						{
-								push(x-1,y,dir);
+								used_hammer=0;
 						}
+						if(dir==-1)
+						{
+								push(x-1,y,MOVE_LEFT,hammer-used_hammer,step+1);
+						}
+						else
+						{
+								push(x-1,y,dir,hammer-used_hammer,step+1);
+						}
+
 				}
-				if(tmp_map[x+1][y]==FREESPACE || tmp_map[x+1][y]==target)
+				if(tmp_map[x+1][y]==FREESPACE || tmp_map[x+1][y]==target|| ( tmp_map[x+1][y]==BLOCK && hammer>0))
 				{
 //						printf("PUSH RIGHT\n");
-						if(dir==-1)
+
+						if(tmp_map[x+1][y]==BLOCK)
 						{
-								push(x+1,y,MOVE_RIGHT);
+								used_hammer=1;
 						}
 						else
 						{
-								push(x+1,y,dir);
+								used_hammer=0;
+						}
+						if(dir==-1)
+						{
+								push(x+1,y,MOVE_RIGHT,hammer-used_hammer,step+1);
+						}
+						else
+						{
+								push(x+1,y,dir,hammer-used_hammer,step+1);
 						}
 				}
-				if(tmp_map[x][y-1]==FREESPACE || tmp_map[x][y-1]==target)
+				if(tmp_map[x][y-1]==FREESPACE || tmp_map[x][y-1]==target|| ( tmp_map[x][y-1]==BLOCK && hammer>0))
 				{
 //						printf("PUSH UP\n");
 
-						if(dir==-1)
+						if(tmp_map[x][y-1]==BLOCK)
 						{
-								push(x,y-1,MOVE_UP);
+								used_hammer=1;
 						}
 						else
 						{
-								push(x,y-1,dir);
+								used_hammer=0;
+						}
+						if(dir==-1)
+						{
+								push(x,y-1,MOVE_UP,hammer-used_hammer,step+1);
+						}
+						else
+						{
+								push(x,y-1,dir,hammer-used_hammer,step+1);
 						}
 				}
-				if(tmp_map[x][y+1]==FREESPACE || tmp_map[x][y+1]==target)
+				if(tmp_map[x][y+1]==FREESPACE || tmp_map[x][y+1]==target|| ( tmp_map[x][y+1]==BLOCK && hammer>0))
 				{
 //						printf("PUSH DOWN\n");
 
-						if(dir==-1)
+						if(tmp_map[x][y+1]==BLOCK)
 						{
-								push(x,y+1,MOVE_DOWN);
+								used_hammer=1;
 						}
 						else
 						{
-								push(x,y+1,dir);
+								used_hammer=0;
+						}
+						if(dir==-1)
+						{
+								push(x,y+1,MOVE_DOWN,hammer-used_hammer,step+1);
+						}
+						else
+						{
+								push(x,y+1,dir,hammer-used_hammer,step+1);
 						}
 				}
 		}
@@ -182,6 +243,10 @@ int next_move(json_t* message)
 		int map_level;
 		int leveling=0;
 		int ret=MOVE_FAIL;
+		int ret_used_hammer;
+		int count=0;
+		int count_used_hammer;
+		int candidation;
 		memset(map,0,MAP_SIZE*MAP_SIZE);
 	
 		make_map(json_object_get(message,"blocks"),BLOCK);
@@ -202,30 +267,62 @@ int next_move(json_t* message)
 		map_level=json_integer_value(json_object_get(player,"map-level"));
 		level=json_integer_value(json_object_get(player,"level"));
 
+		current_map_level=map_level;
+
+#if 0
 		if((num_hammer>0) && ((map_level%5==0) || (num_heal<=1)) )
 		{
 				//破壊にゃんこ
-				ret=search(x,y,BLOCK);
+				ret=search(x,y,BLOCK,num_hammer,&count);
 		}
+#endif
 
-		if((num_items>0) && (ret==MOVE_FAIL) && (num_heal>=1))
+		if((num_items>0) && (ret==MOVE_FAIL))
 		{
 				//お宝ハンターにゃんこ
-				ret=search(x,y,ITEM);
+				ret=search(x,y,ITEM,0,&count);
+				if(num_hammer>0)
+				{
+						ret_used_hammer=search(x,y,ITEM,1,&count_used_hammer);
+						if(count_used_hammer+THRESHOLD_HAMMER<count)
+						{
+								ret=ret_used_hammer;
+								count=count_used_hammer;
+						}
+				}
+				if((num_heal==0) && (count>=THRESHOLD_NEAR_ITEM))
+				{
+						ret=MOVE_FAIL;
+				}
 		}
 
-#if 0
-		if((ret==MOVE_FAIL) &&(num_heal>(map_level/7)+1) && ((map_level%7)==6) && (level<map_level))
+#if 1
+		if((ret==MOVE_FAIL) && (num_heal>=4) && (map_level==6 || map_level==13) && (level<map_level) &&(current_weapon_pow>map_level*1.0))
 		{
 				//レベリングにゃんこ
 				leveling=1;
-				ret=MOVE_FAIL;
+				if(map[x-1][y]==FREESPACE)
+						ret=MOVE_LEFT;
+				else if(map[x+1][y]==FREESPACE)
+						ret=MOVE_RIGHT;
+				else if(map[x][y-1]==FREESPACE)
+						ret=MOVE_UP;
+				else if(map[x][y+1]==FREESPACE)
+						ret=MOVE_DOWN;
 		}
 #endif
-		if((ret==MOVE_FAIL) && (leveling==0))
+		if(((ret==MOVE_FAIL) || (count>=array_threshold_far_item[map_level/10])) && (leveling==0))
 		{
 				//逃亡にゃんこ
-				ret=search(x,y,EVENT);
+				ret=search(x,y,EVENT,0,&count);
+				if(num_hammer>0)
+				{
+						ret_used_hammer=search(x,y,EVENT,1,&count_used_hammer);
+						if(count_used_hammer+THRESHOLD_HAMMER<count)
+						{
+								ret=ret_used_hammer;
+						}
+				}
 		}
 
 		return ret;
@@ -241,7 +338,7 @@ void map_mode(json_t* message)
 		player=json_object_get(message,"player");
 
 //		printf("hp:%lld maxhp:%lld\n",hp,max_hp);
-		if(is_heal(player,3))
+		if(is_heal(player,1))
 		{
 				puts("HEAL");
 		}
@@ -303,12 +400,7 @@ void battle_mode(json_t* message)
 				}
 			}
 		}
-		if(alive_monsters>4 && is_heal(player,2))
-		{
-				puts("HEAL");
-		}
-
-		else if(is_heal(player,3))
+		if(is_heal(player,1+(alive_monsters*0.1)))
 		{
 				puts("HEAL");
 		}
@@ -357,9 +449,14 @@ void equip_mode(json_t* message)
 				sum_now++;
 		}
 		if(sum_now<sum_discover)
+		{
 				puts("YES");
+				current_weapon_pow=sum_discover;
+		}
 		else
+		{
 				puts("NO");
+		}
 
 #if 0
 		printf("%s %lld %lld %lld\n"
